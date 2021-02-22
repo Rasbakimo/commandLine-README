fba22a9f9f5ccc92126c7386299dc9a11ca59f69
Rasbakimo <bakimocleckley@yahoo.com>
2021-02-22 13:56:19 -0500

    works if user has all required info

--- index.js
+++ index.js
@@ -1,60 +1,181 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+// External packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-function promptUser() {
-  return inquirer.prompt([
+// Inquirer prompts for userResponses
+const questions = [
     {
-      type: "input",
-      name: "Github",
-      message: "Github username?"
+        type: 'input',
+        message: "Github user name?",
+        name: 'username',
+        default: 'Rasbakimo-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
     },
     {
-      type: "input",
-      name: "email ",
-      message: "email address?"
+        type: 'input',
+        message: "GitHub repo?",
+        name: 'repo',
+        default: 'commandLine-README',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
     },
     {
-      type: "input",
-      name: "project name",
-      message: "What is the name of your project?"
+        type: 'input',
+        message: "Project title?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
     },
     {
-      type: "input",
-      name: "food",
-      message: "What is your favorite food?"
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
     },
     {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
     },
     {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
     }
-  ]);
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Almost done")
+    });
 }
 
-function generateREADME(answers) {
-  console.log(answers)
-  return `${answers.name}
-  ${answers.location}
-  `;
-}
+const writeFileAsync = util.promisify(writeToFile);
 
-promptUser()
-  .then(function(answers) {
-    const md = generateREADME(answers);
 
-    return writeFileAsync("README.md", md);
-  })
-  .then(function() {
-    console.log("Successfully wrote to index.md");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// Main function
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Retrieving Github data");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log( userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("running data through the markDown")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+init();
+
+
+// {
+//     type: "input",
+//     name: "github",
+//     message: "Enter Github username."
+//   },
+//   {
+//     type: "input",
+//     name: "repo",
+//     message: "Enter your GitHub repo name."
+//   },
+//   {
+//     type: "input",
+//     name: "email ",
+//     message: "Enter email address"
+//   },
+//   {
+//     type: "input",
+//     name: "projectName",
+//     message: "What is the name of your project?"
+//   },
+//   {
+//     type: "input",
+//     name: "description",
+//     message: "what is the project about?"
+//   },
+ 
+//   {
+//     type: "input",
+//     name: "installation",
+//     message: "If necessary, please provide steps for installing."
+//   },
+//   {
+//     type: "input",
+//     name: "usage",
+//     message: "How does the project function ad how is it used?."
+//   },
+//   {
+//     type: "input",
+//     name: "contributions",
+//     message: "How can other developers make contributions to the project?"
+//   },
+//   {
+//     type: "input",
+//     name: "test",
+//     message: "How can this be tested if required?."
+//   },{
+//     type: "list",
+//     name: "license",
+//     message: "Choose a license to use.",
+//     choices: ["MIT License", "GNU LGPLv3", "Apache License 2.0"]
+//   }
+// ];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,132 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+ function generateMarkdown(userResponses, userInfo) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Table of Contents Mkdown
+  let draftToC = `## Table of Contents`;
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (userResponses.installation !== '') { draftToC += `
+  * [Installation](#installation)` };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  if (userResponses.usage !== '') { draftToC += `
+  * [Usage](#usage)` };
 
-`;
-}
+  if (userResponses.contributing !== '') { draftToC += `
+  * [Contributing](#contributing)` };
 
+  if (userResponses.tests !== '') { draftToC += `
+  * [Tests](#tests)` };
+
+
+  // README title and badge logo
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *What's the Objective:* 
+  
+  ${userResponses.description}
+  `
+  // Add ToC
+  draftMarkdown += draftToC;
+ 
+  // add license to ToC
+  draftMarkdown += `
+  * [License](#license)`;
+  
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  * Here's hpw to install it:*
+  
+  ${userResponses.installation}`
+  };
+  // Optional Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+  *Instructions and demo*
+  
+  ${userResponses.usage}`
+  };
+  
+  
+  // Optional Contributing section
+  if (userResponses.contributing !== '') {
+  `
+  
+  ## Contributing
+  
+  *Hers's how to add contributions, please feel free to do so*
+  
+  ${userResponses.contributing}`
+  };
+  
+
+  // Optional Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  *Here's how to test the app:*
+  
+  ${userResponses.tests}`
+  };
+
+
+  // License section is required
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+
+  // Questions / About Developer section
+  let draftDev = 
+  `
+  ---
+  
+  ## Questions?
+  
+  
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+  // If GitHub email is not null, add to Developer section
+  if (userInfo.email !== null) {
+  
+  draftDev +=
+  `
+  Email: ${userInfo.email}
+  `};
+
+  // Add developer section to markdown
+  draftMarkdown += draftDev;
+
+  // Return markdown
+  return draftMarkdown;
+  }
 module.exports = generateMarkdown;
